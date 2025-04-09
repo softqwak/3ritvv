c12b19bd03b5ce2069b8104d1b65b5fe099cb5cd
softqwak <valentin.kuzichev@yandex.ru>
2025-04-10 01:29:07 +0300

    Готово)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tbxInput = new TextBox();
+            label1 = new Label();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            splitContainer1 = new SplitContainer();
+            tbxOutput = new TextBox();
+            tbxError = new TextBox();
+            statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tbxInput
+            // 
+            tbxInput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tbxInput.Location = new Point(13, 35);
+            tbxInput.Name = "tbxInput";
+            tbxInput.Size = new Size(714, 23);
+            tbxInput.TabIndex = 0;
+            tbxInput.TextChanged += tbxInput_TextChanged;
+            tbxInput.KeyPress += tbxInput_KeyPress;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(13, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Ввод";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 560);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(739, 22);
+            statusStrip1.TabIndex = 4;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(144, 17);
+            toolStripStatusLabel1.Text = "Транслятор языка ЛИСП";
+            toolStripStatusLabel1.Click += toolStripStatusLabel1_Click;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            splitContainer1.BorderStyle = BorderStyle.FixedSingle;
+            splitContainer1.Location = new Point(13, 64);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(tbxOutput);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(tbxError);
+            splitContainer1.Size = new Size(714, 493);
+            splitContainer1.SplitterDistance = 246;
+            splitContainer1.TabIndex = 5;
+            // 
+            // tbxOutput
+            // 
+            tbxOutput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tbxOutput.Location = new Point(3, 3);
+            tbxOutput.Multiline = true;
+            tbxOutput.Name = "tbxOutput";
+            tbxOutput.Size = new Size(238, 485);
+            tbxOutput.TabIndex = 0;
+            // 
+            // tbxError
+            // 
+            tbxError.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tbxError.Location = new Point(3, 3);
+            tbxError.Multiline = true;
+            tbxError.Name = "tbxError";
+            tbxError.ScrollBars = ScrollBars.Vertical;
+            tbxError.Size = new Size(456, 485);
+            tbxError.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(739, 582);
+            Controls.Add(splitContainer1);
+            Controls.Add(statusStrip1);
+            Controls.Add(label1);
+            Controls.Add(tbxInput);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "ЛИСП";
+            Load += Form1_Load;
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox tbxInput;
+        private Label label1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private SplitContainer splitContainer1;
+        private TextBox tbxOutput;
+        private TextBox tbxError;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,207 @@
+﻿using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace _3ritvv
 {
     public partial class Form1 : Form
     {
+        bool isGoodParse = false;
+        List<string> errorList = new List<string>() { };
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            // Разрешаем только цифры, буквы, +, *, (, ), пробелы и управляющие символы
+            if (!char.IsControl(ch) && !char.IsDigit(ch) && !char.IsLetter(ch) && ch != '+' && ch != '*' && ch != '(' && ch != ')' && ch != ' ')
+            {
+                e.Handled = true; // Если символ не разрешён, игнорируем его
+            }
+        }
+
+        private void tbxInput_TextChanged(object sender, EventArgs e)
+        {
+            string input = tbxInput.Text;
+            var tokens = Tokenize(input);
+            var parsed = Parse(tokens);
+            double result = Eval(parsed);
+            tbxOutput.Text = result.ToString();
+
+            if (isGoodParse)
+            {
+                errorList.Clear();
+                tbxError.Text = "";
+            }
+            else
+            { 
+                // Переворачиваем список ошибок
+                errorList.Reverse();
+
+                // Выводим ошибки в TextBox в обратном порядке
+                tbxError.Text = string.Join(Environment.NewLine, errorList);
+                tbxOutput.Text = "";
+            }
+            isGoodParse = true;
+
+        }
+
+        private List<string> Tokenize(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return new List<string>();
+
+            input = input.Replace("(", " ( ").Replace(")", " ) ");
+            return input
+                .Split(new[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+        }
+
+        private Expr? Parse(List<string> tokens)
+        {
+            if (tokens.Count == 0)
+                return null;
+
+            try
+            {
+                return ParseExpr(tokens);
+            }
+            catch (Exception ex)
+            {
+                isGoodParse = false;
+                return null;
+            }
+        }
+
+        private Expr ParseExpr(List<string> tokens)
+        {
+            if (tokens.Count == 0)
+            {
+                isGoodParse = false;
+                errorList.Add(DateTime.Now.ToString("HH:mm:ss") + $" <{tbxInput.Text}> " + new Exception("Ожидался токен, но достигнут конец ввода.").Message);
+            }
+
+            string token = tokens[0];
+            tokens.RemoveAt(0);
+
+            if (token == "(")
+            {
+                var elements = new List<Expr>();
+
+                while (tokens.Count > 0 && tokens[0] != ")")
+                {
+                    elements.Add(ParseExpr(tokens));
+                }
+
+                if (tokens.Count == 0)
+                {
+                    isGoodParse = false;
+                    errorList.Add(DateTime.Now.ToString("HH:mm:ss") + $" <{tbxInput.Text}> " + new Exception("Пропущена закрывающая скобка \')\'.").Message);
+                }
+
+                tokens.RemoveAt(0); // Удаляем ')'
+                return new ListExpr(elements);
+            }
+            else if (token == ")")
+            {
+                isGoodParse = false;
+                errorList.Add(DateTime.Now.ToString("HH:mm:ss") + $" <{tbxInput.Text}> " + new Exception("Неожиданная закрывающая скобка \')\'.").Message);
+            }
+            else
+            {
+                return Atom(token);
+            }
+            return null;
+        }
+
+        private Expr Atom(string token)
+        {
+            if (double.TryParse(token, out double number))
+                return new Number(number);
+
+            return new Symbol(token);
+        }
+
+        private double Eval(Expr expr)
+        {
+            if (expr is Number num)
+                return num.Value;
+
+            if (expr is ListExpr list)
+            {
+                if (list.Elements.Count == 0)
+                {
+                    isGoodParse = false;
+                    errorList.Add(DateTime.Now.ToString("HH:mm:ss") + $" <{tbxInput.Text}> " + new Exception("Пустое выражение.").Message);
+                    return 0;
+                }
+
+                if (list.Elements[0] is Symbol op)
+                {
+                    var args = list.Elements.Skip(1).Select(Eval).ToList();
+
+                    if (op.Name == "+")
+                    {
+                        return args.Sum();
+                    }
+                    else if (op.Name == "*")
+                    {
+                        return args.Aggregate(1.0, (acc, x) => acc * x);
+                    }
+                    else
+                    {
+                        isGoodParse = false;
+                        errorList.Add(DateTime.Now.ToString("HH:mm:ss") + $" <{tbxInput.Text}> " + $"Неизвестный оператор {op.Name}.");
+                        return 0;  // Или выбросить исключение
+                    }
+                }
+                else
+                {
+                    isGoodParse = false;
+                    errorList.Add(DateTime.Now.ToString("HH:mm:ss") + $" <{tbxInput.Text}> " + new Exception("Первый символ в списке не оператор.").Message);
+                }
+            }
+            isGoodParse = false;
+            errorList.Add(DateTime.Now.ToString("HH:mm:ss") + $" <{tbxInput.Text}> " + new Exception("Неизвестное выражение.").Message);
+            return 0;
+        }
+    }
+    abstract class Expr { }
+
+    class Number : Expr
+    {
+        public double Value;
+        public Number(double value) => Value = value;
+    }
+
+    class Symbol : Expr
+    {
+        public string Name;
+        public Symbol(string name) => Name = name;
+    }
+
+    class ListExpr : Expr
+    {
+        public List<Expr> Elements;
+        public ListExpr(List<Expr> elements) => Elements = elements;
     }
 }
